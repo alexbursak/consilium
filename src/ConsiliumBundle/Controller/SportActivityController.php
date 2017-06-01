43ade5b8fb519cdb8aafa5c9112f22c50c5cdf9c
alexbursak <alexbursak3@gmail.com>
2017-06-01 23:23:27 +0100

    implemented Sport activity crud

--- src/ConsiliumBundle/Controller/SportActivityController.php
+++ src/ConsiliumBundle/Controller/SportActivityController.php
@@ -2,8 +2,11 @@
 
 namespace ConsiliumBundle\Controller;
 
+use ConsiliumBundle\Entity\Day;
 use ConsiliumBundle\Entity\SportActivity;
+use ConsiliumBundle\Validator\SportActivityValidator;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -18,38 +21,162 @@ class SportActivityController extends Controller
      */
     public function indexAction()
     {
-        return new JsonResponse('indexAction');
+        $sportActivities = $this->getDoctrine()->getRepository('ConsiliumBundle:SportActivity')->findAll();
+
+        if(empty($sportActivities)){
+            return new JsonResponse([]);
+        }
+
+        return new Response($this->serializeJson($sportActivities), 200, ['Content-type' => 'application/json']);
     }
 
     /**
      * Creates a new SportActivity entity.
+     *
+     * @param Request $request
+     *
+     * @return JsonResponse
      */
     public function newAction(Request $request)
     {
-        return new JsonResponse('newAction');
+        $validator = new SportActivityValidator($request->getContent());
+        $validator->validate();
+
+        if(!$validator->isValid()){
+            return new JsonResponse($validator->getErrors());
+        }
+
+        $em = $this->getDoctrine()->getManager();
+
+        $data = json_decode($request->getContent());
+
+        $sportActivity = new SportActivity();
+        $sportActivity->setTitle($data->title);
+        $sportActivity->setReps($data->reps);
+        $sportActivity->setSets($data->sets);
+        $sportActivity->setWeight($data->weight);
+
+        $day = $this->getDoctrine()
+            ->getRepository('ConsiliumBundle:Day')
+            ->findDayByDate($data->date);
+        if(null === $day){
+            $day = new Day();
+            $day->setDate(\DateTime::createFromFormat('d-m-Y', $data->date));
+        }
+        $sportActivity->setDay($day);
+
+        // TODO: rename 'note' field!
+        $activityType = $this->getDoctrine()
+            ->getRepository('ConsiliumBundle:ActivityType')
+            ->findOneBy(['note' => $data->type]);
+        if(null === $activityType){
+            return new JsonResponse('ERROR - Activity type not found');
+        }
+        $activityType->addSportActivity($sportActivity);
+        $sportActivity->setType($activityType);
+
+        $em->persist($sportActivity);
+        $em->persist($day);
+        $em->persist($activityType);
+
+        try{
+            $em->flush();
+        }catch (\Exception $e){
+            return new JsonResponse('ERROR - failed write to DB - code: ' . $e->getCode());
+        }
+
+        return new JsonResponse('New SportActivity created');
     }
 
     /**
      * Finds and displays a SportActivity entity.
+     *
+     * @param SportActivity $sportActivity
+     *
+     * @return Response
      */
-    public function showAction(SportActivity $day)
+    public function showAction(SportActivity $sportActivity)
     {
-        return new JsonResponse('showAction');
+        return new Response($this->serializeJson($sportActivity), 200, ['Content-type' => 'application/json']);
     }
 
     /**
      * Displays a form to edit an existing SportActivity entity.
      */
-    public function editAction(Request $request, SportActivity $day)
+    public function editAction(Request $request, SportActivity $sportActivity)
     {
-        return new JsonResponse('editAction');
+        $validator = new SportActivityValidator($request->getContent());
+        $validator->validate();
+
+        if(!$validator->isValid()){
+            return new JsonResponse($validator->getErrors());
+        }
+
+        $em = $this->getDoctrine()->getManager();
+
+        $data = json_decode($request->getContent());
+
+        if(null === $sportActivity){
+            return new JsonResponse('ERROR - Activity not found');
+        }
+        $sportActivity->setTitle($data->title);
+        $sportActivity->setReps($data->reps);
+        $sportActivity->setSets($data->sets);
+        $sportActivity->setWeight($data->weight);
+
+        $day = $this->getDoctrine()
+            ->getRepository('ConsiliumBundle:Day')
+            ->findDayByDate($data->date);
+        if(null === $day){
+            $day = new Day();
+            $day->setDate(\DateTime::createFromFormat('d-m-Y', $data->date));
+        }
+        $sportActivity->setDay($day);
+
+        // TODO: rename 'note' field!
+        $activityType = $this->getDoctrine()
+            ->getRepository('ConsiliumBundle:ActivityType')
+            ->findOneBy(['note' => $data->type]);
+        if(null === $activityType){
+            return new JsonResponse('ERROR - Activity type not found');
+        }
+        $activityType->addSportActivity($sportActivity);
+        $sportActivity->setType($activityType);
+
+        $em->persist($sportActivity);
+        $em->persist($day);
+        $em->persist($activityType);
+
+        try{
+            $em->flush();
+        }catch (\Exception $e){
+            return new JsonResponse('ERROR - failed write to DB - code: ' . $e->getCode());
+        }
+
+        return new JsonResponse('SportActivity edited');
     }
 
     /**
-     * Deletes a SportActivity entity.
+     * @param SportActivity $sportActivity
+     *
+     * @return JsonResponse
      */
-    public function deleteAction(Request $request)
+    public function deleteAction(SportActivity $sportActivity)
     {
-        return new JsonResponse('deleteAction');
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($sportActivity);
+
+        try{
+            $em->flush();
+        }catch (\Exception $e){
+            return new JsonResponse('ERROR - failed write to DB - code: ' . $e->getCode());
+        }
+
+        return new JsonResponse('SportActivity has been deleted');
+    }
+
+    private function serializeJson($data)
+    {
+        return $this->get('serializer')->serialize($data, 'json');
     }
 }
