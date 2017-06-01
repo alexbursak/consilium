684ba36eaf79f9e1973191aaf5ac37c045c31135
alexbursak <alexbursak3@gmail.com>
2017-06-01 23:47:47 +0100

    implemented activity type api crud (prototype)

--- src/ConsiliumBundle/Controller/ActivityTypeController.php
+++ src/ConsiliumBundle/Controller/ActivityTypeController.php
@@ -3,9 +3,11 @@
 namespace ConsiliumBundle\Controller;
 
 use ConsiliumBundle\Entity\ActivityType;
+use ConsiliumBundle\Validator\ActivityTypeValidator;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * ActivityType controller.
@@ -14,41 +16,123 @@ class ActivityTypeController extends Controller
 {
     /**
      * Lists all ActivityType entities.
+     *
+     * @return JsonResponse|Response
      */
     public function indexAction()
     {
-        return new JsonResponse('indexAction');
+        $days = $this->getDoctrine()->getRepository('ConsiliumBundle:ActivityType')->findAll();
+
+        if(empty($days)){
+            return new JsonResponse([]);
+        }
+
+        return new Response($this->serializeJson($days), 200, ['Content-type' => 'application/json']);
     }
 
     /**
      * Creates a new ActivityType entity.
+     *
+     * @param Request $request
+     *
+     * @return JsonResponse
      */
     public function newAction(Request $request)
     {
-        return new JsonResponse('newAction');
+        $validator = new ActivityTypeValidator($request->getContent());
+        $validator->validate();
+
+        if(!$validator->isValid()){
+            return new JsonResponse($validator->getErrors());
+        }
+
+        $data = json_decode($request->getContent());
+
+        $activityType = new ActivityType();
+        $activityType->setNote($data->note);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($activityType);
+
+        try{
+            $em->flush();
+        }catch (\Exception $e){
+            return new JsonResponse('ERROR - failed write to DB - code: ' . $e->getCode());
+        }
+
+        return new JsonResponse('New ActivityType created');
     }
 
     /**
      * Finds and displays a ActivityType entity.
+     *
+     * @param ActivityType $activityType
+     *
+     * @return Response
      */
-    public function showAction(ActivityType $day)
+    public function showAction(ActivityType $activityType)
     {
-        return new JsonResponse('showAction');
+        return new Response($this->serializeJson($activityType), 200, ['Content-type' => 'application/json']);
     }
 
     /**
      * Displays a form to edit an existing ActivityType entity.
+     *
+     * @param Request $request
+     * @param ActivityType $activityType
+     *
+     * @return JsonResponse
      */
-    public function editAction(Request $request, ActivityType $day)
+    public function editAction(Request $request, ActivityType $activityType)
     {
-        return new JsonResponse('editAction');
+        $data = $request->getContent();
+
+        $validator = new ActivityTypeValidator($data);
+        $validator->validate();
+
+        if(!$validator->isValid()){
+            return new JsonResponse($validator->getErrors());
+        }
+
+        $data = json_decode($data);
+
+        $activityType->setNote($data->note);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($activityType);
+
+        try{
+            $em->flush();
+        }catch (\Exception $e){
+            return new JsonResponse('ERROR - failed write to DB - code: ' . $e->getCode());
+        }
+
+        return new JsonResponse('ActivityType edited');
     }
 
     /**
      * Deletes a ActivityType entity.
+     *
+     * @param ActivityType $activityType
+     *
+     * @return JsonResponse
      */
-    public function deleteAction(Request $request)
+    public function deleteAction(ActivityType $activityType)
     {
-        return new JsonResponse('deleteAction');
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($activityType);
+
+        try{
+            $em->flush();
+        }catch (\Exception $e){
+            return new JsonResponse('ERROR - failed write to DB - code: ' . $e->getCode());
+        }
+
+        return new JsonResponse('ActivityType has been deleted');
+    }
+
+    private function serializeJson($data)
+    {
+        return $this->get('serializer')->serialize($data, 'json');
     }
 }
---
+++ src/ConsiliumBundle/Validator/ActivityTypeValidator.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace ConsiliumBundle\Validator;
+
+
+class ActivityTypeValidator
+{
+    const REQUIRED_FIELDS = [
+        'note',
+    ];
+
+    /**
+     * Raw data from Request
+     *
+     * @var mixed
+     */
+    private $data;
+
+    /**
+     * @var array
+     */
+    private $errors = [];
+
+    /**
+     * @var bool
+     */
+    private $valid = false;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Validates data format (basic)
+     *
+     * @return $this
+     */
+    public function validate()
+    {
+        if (empty($this->data)) {
+            array_push($this->errors, 'NO DATA');
+            return $this;
+        }
+
+        $dataArray = (array)json_decode($this->data);
+
+        if ($this->checkRequiredKeysExists($dataArray)){
+            foreach ($dataArray as $fiendsName => $field){
+                if(empty($field)){
+                    array_push($this->errors,  sprintf('EMPTY FIELD \'%s\'', $fiendsName));
+                }
+            }
+        }else{
+            array_push($this->errors,  'REQUIRED FIELD(S) MISSING');
+        }
+
+        if(empty($this->errors)){
+            $this->valid = true;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param array $dataArray
+     *
+     * @return bool
+     */
+    private function checkRequiredKeysExists(array $dataArray)
+    {
+            return !array_diff_key(array_flip(self::REQUIRED_FIELDS), $dataArray);
+    }
+
+    /**
+     * @return array
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isValid()
+    {
+        return $this->valid;
+    }
+}
